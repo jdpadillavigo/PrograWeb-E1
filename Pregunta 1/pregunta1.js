d3aeacb21e48408005aa2d2b551171f6047b2dba
Juan Diego <172089930+jdpadillavigo@users.noreply.github.com>
2025-05-25 18:08:30 -0500

    Pregunta 1 terminada y las demás fueron mejoradas

--- Pregunta 1/pregunta1.js
+++ Pregunta 1/pregunta1.js
@@ -1,13 +1,34 @@
 const butAddOnClick = function() {
-    const inputArea = document.getElementById("input-area")
-    inputArea.setAttribute("class", "show-menu")
-    // const newLi = document.createElement('li');
-    // newLi.textContent = input.value;
+    const inputText = document.getElementById("input-text")
+    const msg = document.getElementById("message")
+    if (inputText.value == "") {
+        msg.style.display = "block"
+        setTimeout(() => {
+            msg.style.display = "none"
+        }, 5000)
+    } else {
+        msg.style.display = "none"
+        const menuList = document.getElementById("menu-list")
+        const newLi = document.createElement('li')
+        newLi.innerHTML = inputText.value
+        menuList.appendChild(newLi)
+    }
+}
+
+const butMenuOnClick = function() {
+    const list = document.getElementById("list")
+    if (list.style.display == "none") {
+        list.style.display = "block"
+    } else {
+        list.style.display = "none"   
+    }
 }
 
 const main = function() {
     const butAdd = document.getElementById("but-add")
     butAdd.addEventListener("click", butAddOnClick)
+    const butMenu = document.getElementById("but-menu")
+    butMenu.addEventListener("click", butMenuOnClick)
 }
 
 main()
--- Pregunta 2/pregunta2.js
+++ Pregunta 2/pregunta2.js
@@ -1,70 +1,50 @@
-const errorMessage = "Falta ingresar valores"
-
-const butAddOnClick = function() {
+const calculator = (operation) => {
     const number1 = document.getElementById("n1")
     const number2 = document.getElementById("n2")
+    const msg = document.getElementById("message")
+    msg.style.display = "none"
     if (number1.value == "" ||
         number2.value == ""
     ) {
-        console.error(errorMessage)
+        result.value = ""
+        msg.innerText = "¡Falta ingresar valores!"
+        msg.style.display = "block"
+        setTimeout(() => {
+            msg.style.display = "none"
+        }, 5000)
     } else {
-        const operation = parseFloat(number1.value) + parseFloat(number2.value) 
         const result = document.getElementById("result")
-        result.value = operation
+        if (operation == "add") {
+            result.value = parseFloat(number1.value) + parseFloat(number2.value)
+            msg.style.display = "none"
+        } else if (operation == "subtract") {
+            result.value = parseFloat(number1.value) - parseFloat(number2.value)
+            msg.style.display = "none"
+        } else if (operation == "multiply") {
+            result.value = parseFloat(number1.value) * parseFloat(number2.value)
+            msg.style.display = "none"
+        } else if (operation == "divide") {
+            if (parseFloat(number2.value) === 0) {
+                result.value = ""
+                msg.innerText = "¡No se puede dividir entre 0!"
+                msg.style.display = "block"
+            } else {
+                result.value = parseFloat(number1.value) / parseFloat(number2.value)
+                msg.style.display = "none"
+            }
+        }
     }
 }
 
-const butSubstractOnClick = function() {
-    const number1 = document.getElementById("n1")
-    const number2 = document.getElementById("n2")
-    if (number1.value == "" ||
-        number2.value == ""
-    ) {
-        console.error(errorMessage)
-    } else {
-        const operation = parseFloat(number1.value) - parseFloat(number2.value) 
-        const result = document.getElementById("result")
-        result.value = operation
-    }
-}
-
-const butMultiplyOnClick = function() {
-    const number1 = document.getElementById("n1")
-    const number2 = document.getElementById("n2")
-    if (number1.value == "" ||
-        number2.value == ""
-    ) {
-        console.error(errorMessage)
-    } else {
-        const operation = parseFloat(number1.value) * parseFloat(number2.value) 
-        const result = document.getElementById("result")
-        result.value = operation
-    }
-}
-
-const butDivideOnClick = function() {
-    const number1 = document.getElementById("n1")
-    const number2 = document.getElementById("n2")
-    if (number1.value == "" ||
-        number2.value == ""
-    ) {
-        console.error(errorMessage)
-    } else {
-        const operation = parseFloat(number1.value) / parseFloat(number2.value) 
-        const result = document.getElementById("result")
-        result.value = operation
-    }
-}
-
-const main = function() {
+const main = () => {
     const butAdd = document.getElementById("but-add")
-    const butSubstract = document.getElementById("but-substract")
+    const butSubtract = document.getElementById("but-subtract")
     const butMultiply = document.getElementById("but-multiply")
     const butDivide = document.getElementById("but-divide")
-    butAdd.addEventListener("click", butAddOnClick)
-    butSubstract.addEventListener("click", butSubstractOnClick)
-    butMultiply.addEventListener("click", butMultiplyOnClick)
-    butDivide.addEventListener("click", butDivideOnClick)
+    butAdd.addEventListener("click", () => calculator("add"))
+    butSubtract.addEventListener("click", () => calculator("subtract"))
+    butMultiply.addEventListener("click", () => calculator("multiply"))
+    butDivide.addEventListener("click", () => calculator("divide"))
 }
 
 main()
